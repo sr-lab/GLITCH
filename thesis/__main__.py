7b44c1581c50b951e41eb4c6a9ce880c779a0b04
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-03-05 15:50:10 +0000

    Add click to create command line tool

--- thesis/__main__.py
+++ thesis/__main__.py
@@ -1,4 +1,17 @@
-from thesis.parsers.cmof import ChefParser
+import click
 
-parser = ChefParser()
-print(parser.parse("thesis/examples/chef/nginx"))
+from thesis.parsers.cmof import AnsibleParser, ChefParser
+
+@click.command()
+@click.option('--tech',
+        type=click.Choice(['ansible', 'chef'], case_sensitive=False), required=True)
+@click.argument('path', type=click.Path(exists=True))
+def analysis(tech, path):
+    parser = None
+    if tech == "ansible":
+        parser = AnsibleParser()
+    elif tech == "chef":
+        parser = ChefParser()
+    print(parser.parse(path))
+
+analysis(prog_name='#TODO')
