3108f376fc44eea1a1792117f840647c581fd368
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-05-27 15:11:25 +0100

    Force click version

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     install_requires=[
         "ruamel.yaml",
         "ply",
-        "click",
+        "click==7.1.2",
         "alive-progress",
         "prettytable",
         "pandas"
