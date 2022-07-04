f4ebb7d68b9d33ae4c4265fb49ae55a4f5e40932
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2022-07-04 15:13:04 +0100

    Ansible - suspicious comment, default admin and empty pass tests

--- glitch/tests/security/ansible/test_security.py
+++ glitch/tests/security/ansible/test_security.py
@@ -11,6 +11,7 @@ class TestSecurity(unittest.TestCase):
         analysis = SecurityVisitor(Tech.ansible)
         analysis.config("configs/default.ini")
         errors = list(set(analysis.check(inter)))
+        errors = sorted(errors, key=lambda e: (e.path, e.line, e.code))
         self.assertEqual(len(errors), n_errors)
         for i in range(n_errors):
             self.assertEqual(errors[i].code, codes[i])
@@ -21,4 +22,25 @@ class TestSecurity(unittest.TestCase):
             "tests/security/ansible/files/http.yml",
             "tasks",
             1, ["sec_https"], [4]
+        )
+
+    def test_susp_comment(self):
+        self.__help_test(
+            "tests/security/ansible/files/susp.yml",
+            "vars",
+            1, ["sec_susp_comm"], [9]
+        )
+
+    def test_def_admin(self):
+        self.__help_test(
+            "tests/security/ansible/files/admin.yml",
+            "tasks",
+            3, ["sec_def_admin", "sec_hard_secr", "sec_hard_user"], [3, 3, 3]
+        )
+
+    def test_empt_pass(self):
+        self.__help_test(
+            "tests/security/ansible/files/empty.yml",
+            "tasks",
+            3, ["sec_empty_pass", "sec_hard_pass", "sec_hard_secr"], [8, 8, 8]
         )
