f7a12ed161c6eddeb7f71747526eb5690ffc59b1
Nfsaavedra <nuno.saavedra@tecnico.ulisboa.pt>
2024-03-19 20:55:51 +0000

    run black on scripts folder

--- scripts/docker_images_scraper.py
+++ scripts/docker_images_scraper.py
@@ -1,18 +1,15 @@
 import requests
 
 next_url = "https://hub.docker.com/api/content/v1/products/search?image_filter=official&page=1&page_size=100&q=&type=image"
-headers = {
-    "Accept": "application/json",
-    "Search-Version": "v3"
-}
+headers = {"Accept": "application/json", "Search-Version": "v3"}
 
 images_list = []
 
 while next_url:
     res = requests.get(next_url, headers=headers).json()
-    next_url = res['next']
-    images = [i['name'] for i in res['summaries']]
+    next_url = res["next"]
+    images = [i["name"] for i in res["summaries"]]
     images_list += images
 
-with open("official_images", 'w') as f:
+with open("official_images", "w") as f:
     f.write("\n".join(images_list))
--- scripts/obsolete_commads_scraper.py
+++ scripts/obsolete_commads_scraper.py
@@ -5,9 +5,9 @@ URL = "https://docstore.mik.ua/orelly/unix3/unixnut/appb_02.htm"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
-table = soup.find('table', cellpadding="5", border="1")
-tds = table.find_all('td', valign='top')
+table = soup.find("table", cellpadding="5", border="1")
+tds = table.find_all("td", valign="top")
 obsolete = [td.text for td in tds if len(td) == 1]
 
-with open("obsolete_commands", 'w') as f:
+with open("obsolete_commands", "w") as f:
     f.write("\n".join(obsolete))
