899368c8bfeab0567980c3eb7c5cecba0058561a
xCoolHat <74071118+sfondev@users.noreply.github.com>
2025-06-28 00:29:16 +0100

    fix docker_images_scraper.py: add support for the new search api and add new filtering for official images that are deprecated

--- scripts/docker_images_scraper.py
+++ scripts/docker_images_scraper.py
@@ -1,15 +1,28 @@
 import requests
 
-next_url = "https://hub.docker.com/api/content/v1/products/search?image_filter=official&page=1&page_size=100&q=&type=image"
-headers = {"Accept": "application/json", "Search-Version": "v3"}
+next_url = "https://hub.docker.com/api/search/v4?badges=official&size=100&query=&type=image"
+headers = {"Accept": "application/json"}
 
+ses = requests.Session()
 images_list = []
+deprecated = []
+res = ses.get(next_url, headers=headers).json()
+total = res["total"]
+current = 0
 
-while next_url:
-    res = requests.get(next_url, headers=headers).json()
-    next_url = res["next"]
-    images = [i["name"] for i in res["summaries"]]
-    images_list += images
+while current < total:
+    for elem in res["results"]:
+        if "deprecate" in elem["short_description"].lower():
+            deprecated.append(elem["name"])
+        else:   
+            images_list.append(elem["name"])
+    current += len(res["results"])
+    if current < total:
+        res = ses.get(next_url + f"&from={current}", headers=headers).json()
 
 with open("official_images", "w") as f:
     f.write("\n".join(images_list))
+
+with open("official_deprecated_images", "w") as f:
+    f.write("\n".join(deprecated))
+
